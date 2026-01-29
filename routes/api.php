dea963838d61a4b125521c92eb30ab1f84c8c265
yensel leon <yleon@initiumsoft.com>
2026-01-29 08:24:18 -0400

    feat: implement Author CRUD with role-based access control. Added complete CRUD for Authors with pagination and standardized JSON responses. Implemented ACL to restrict write operations admins

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,14 @@ Route::middleware(['auth:api', 'throttle:60,1', 'role:admin'])->group(function (
     Route::get('users', 'UserController@index');
     Route::delete('users/{user}', 'UserController@destroy');
 });
+
+Route::middleware(['auth:api', 'throttle:60,1'])->group(function () {
+    Route::get('authors', 'AuthorController@index');
+    Route::get('authors/{author}', 'AuthorController@show');
+});
+
+Route::middleware(['auth:api', 'throttle:60,1', 'role:admin'])->group(function () {
+    Route::post('authors', 'AuthorController@store');
+    Route::put('authors/{author}', 'AuthorController@update');
+    Route::delete('authors/{author}', 'AuthorController@destroy');
+});
